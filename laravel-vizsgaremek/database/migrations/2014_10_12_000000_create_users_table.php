c3ec24b826a5a885775d566971356ae166483036
nyikonzoltanferenc <33204609+nyikonzoli@users.noreply.github.com>
2022-02-13 17:23:56 +0100

    user migration

--- laravel-vizsgaremek/database/migrations/2014_10_12_000000_create_users_table.php
+++ laravel-vizsgaremek/database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,10 +17,11 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->date('birthdate');
+            $table->string('address')->nullable();
+            $table->string('profilePictureURI');
+            $table->string('description')->nullable();
         });
     }
 
