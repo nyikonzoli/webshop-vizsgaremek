761116918f9e75951b20ecf10e214b90e9acc85e
nyikonzoltanferenc <33204609+nyikonzoli@users.noreply.github.com>
2022-02-18 10:44:15 +0100

    message index

--- laravel-vizsgaremek/app/Http/Controllers/MessageController.php
+++ laravel-vizsgaremek/app/Http/Controllers/MessageController.php
@@ -3,8 +3,69 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Message;
+use App\Http\Resources\MessageResource;
+use App\Http\Requests\MessageStoreRequest;
 
 class MessageController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $messages = Message::all();
+        $retval = [];
+        foreach ($messages as $message) {
+            $retval[] = new MessageResource($message);
+        }
+        return $retval;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
 }
