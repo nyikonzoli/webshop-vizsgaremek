3fd3bdc49711ea18d6e1c1ea16d01267911c3f7f
nyikonzoltanferenc <33204609+nyikonzoli@users.noreply.github.com>
2022-02-18 15:57:20 +0100

    conversation index

--- laravel-vizsgaremek/app/Http/Controllers/ConversationController.php
+++ laravel-vizsgaremek/app/Http/Controllers/ConversationController.php
@@ -3,8 +3,69 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\ConversationStoreRequest;
+use App\Http\Resources\ConversationResource;
+use App\Models\Conversation;
 
 class ConversationController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $conversations = Conversation::all();
+        $retval = [];
+        foreach ($conversations as $conversation) {
+            $retval[] = new ConvsersationResource($conversation);
+        }
+        return $retval;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
 }
