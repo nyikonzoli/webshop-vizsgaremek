45225376f0386f4d17aa32c39726d45607b8511e
nyikonzoltanferenc <33204609+nyikonzoli@users.noreply.github.com>
2022-02-18 15:56:56 +0100

    conversation resource

---
+++ laravel-vizsgaremek/app/Http/Resources/ConversationResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ConversationResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            "id" => $this->id,
+            "user1Id" => $this->user1Id,
+            "user2Id" => $this->user2Id,
+        ];
+    }
+}
