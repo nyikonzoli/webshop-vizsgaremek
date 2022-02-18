f49523ea8764dc1109c82f10c59c98c789fd7dd5
nyikonzoltanferenc <33204609+nyikonzoli@users.noreply.github.com>
2022-02-18 10:39:46 +0100

    message resource

--- laravel-vizsgaremek/app/Models/Message.php
+++ laravel-vizsgaremek/app/Models/Message.php
@@ -7,5 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Message extends Model
 {
-    use HasFactory;
+    protected $table = "messages";
+
+    protected $fillable = [
+        "conversationId",
+        "content",
+        "date",
+        "userId"
+    ];
 }
