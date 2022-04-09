5f96c3d61303583be70726d6c9547b281d25b1d8
BabikValter <babik.valter@njit.hu>
2022-04-09 19:30:19 +0200

    artshop css

--- laravel-vizsgaremek/resources/views/layouts/main.blade.php
+++ laravel-vizsgaremek/resources/views/layouts/main.blade.php
@@ -8,6 +8,10 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="{{ asset("css/artshop.css") }}">
+
     <title>@yield('title')</title>
     @yield('header')
 </head>
