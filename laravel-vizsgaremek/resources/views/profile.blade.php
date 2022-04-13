003f55af93f3959797966f33b0e4e5fb79eb7b2d
BabikValter <babik.valter@njit.hu>
2022-04-13 04:53:32 +0200

    carousel >:(

--- laravel-vizsgaremek/resources/views/profile.blade.php
+++ laravel-vizsgaremek/resources/views/profile.blade.php
@@ -42,6 +42,32 @@
                         <div class="row g-0">
                             <div class="col-lg-3">
                                 <img src="{{ asset($p->imageConnection->first()->imageURI) }}" alt="" class="img-fluid" style="object-fit: cover; width: 100%; height: 280px;">
+
+
+{{--                                <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">--}}
+{{--                                    <div class="carousel-indicators">--}}
+{{--                                        @for($i = 0; $i < $p->count(); $i++)--}}
+{{--                                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="{{ $i }}" aria-label="Slide {{ ++$i }}"></button>--}}
+{{--                                        @endfor--}}
+{{--                                    </div>--}}
+{{--                                    <div class="carousel-inner">--}}
+{{--                                        @foreach($p->imageConnection as $i)--}}
+{{--                                        <div class="carousel-item">--}}
+{{--                                            <img src="{{ $i->imageUri }}" class="d-block w-100" alt="...">--}}
+{{--                                        </div>--}}
+{{--                                        @endforeach--}}
+{{--                                    </div>--}}
+{{--                                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">--}}
+{{--                                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>--}}
+{{--                                        <span class="visually-hidden">Previous</span>--}}
+{{--                                    </button>--}}
+{{--                                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">--}}
+{{--                                        <span class="carousel-control-next-icon" aria-hidden="true"></span>--}}
+{{--                                        <span class="visually-hidden">Next</span>--}}
+{{--                                    </button>--}}
+{{--                                </div>--}}
+
+
                             </div>
                             <div class="col-lg-9">
                                 <div class="card-body" style="height: 100%;">
