baee011588b4ea0b057257b79118ea0e2643160b
BabikValter <babik.valter@njit.hu>
2022-04-28 19:00:11 +0200

    upload auth fix

--- laravel-vizsgaremek/app/Http/Controllers/UserController.php
+++ laravel-vizsgaremek/app/Http/Controllers/UserController.php
@@ -51,7 +51,7 @@ class UserController extends Controller
     }
 
     public function upload($id) {
-        Gate::authorize('user-views', $id);
+        Gate::authorize('user-privileges', $id);
         return view('profile.upload',[
             'title' => "Upload product",
         ]);
